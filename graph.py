bf93c6cdfca14be3108f6fbfe93dca70fa35c06e
JobeBury <148692121+JobeBury@users.noreply.github.com>
2023-11-21 08:22:40 +0000

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,120 @@
+from configparser import SectionProxy
+from azure.identity import DeviceCodeCredential
+from msgraph import GraphServiceClient
+from msgraph.generated.users.item.mail_folders.item.move.move_post_request_body import MovePostRequestBody
+from msgraph.generated.users.item.user_item_request_builder import UserItemRequestBuilder
+from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import (
+    MessagesRequestBuilder)
+from msgraph.generated.users.item.send_mail.send_mail_post_request_body import (
+    SendMailPostRequestBody)
+from msgraph.generated.models.message import Message
+from msgraph.generated.models.item_body import ItemBody
+from msgraph.generated.models.body_type import BodyType
+from msgraph.generated.models.recipient import Recipient
+from msgraph.generated.models.email_address import EmailAddress
+from msgraph.generated.users.users_request_builder import UsersRequestBuilder
+
+
+class Graph:
+    settings: SectionProxy
+    device_code_credential: DeviceCodeCredential
+    user_client: GraphServiceClient
+
+    def __init__(self, config: SectionProxy):
+        self.settings = config
+        client_id = self.settings['clientId']
+        tenant_id = self.settings['tenantId']
+        graph_scopes = self.settings['graphUserScopes'].split(' ')
+        self.base_url = "https://graph.microsoft.com/v1.0/me/messages"
+        self.device_code_credential = DeviceCodeCredential(client_id, tenant_id=tenant_id)
+        self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
+
+    async def get_user_token(self):
+        graph_scopes = self.settings['graphUserScopes']
+        access_token = self.device_code_credential.get_token(graph_scopes)
+        return access_token.token
+
+    async def get_user(self):
+        # Only request specific properties using $select
+        query_params = UserItemRequestBuilder.UserItemRequestBuilderGetQueryParameters(
+            select=['displayName', 'mail', 'userPrincipalName']
+        )
+
+        request_config = UserItemRequestBuilder.UserItemRequestBuilderGetRequestConfiguration(
+            query_parameters=query_params
+        )
+
+        user = await self.user_client.me.get(request_configuration=request_config)
+        return user
+
+    #####################################################################################################
+    async def get_inbox(self):
+        query_params = MessagesRequestBuilder.MessagesRequestBuilderGetQueryParameters(
+            # Only request specific properties
+            select=['from', 'isRead', 'receivedDateTime', 'subject', 'hasAttachments'],
+            # Get at most 25 results
+            top=25,
+            # Sort by received time, newest first
+            orderby=['receivedDateTime DESC']
+        )
+        request_config = MessagesRequestBuilder.MessagesRequestBuilderGetRequestConfiguration(
+            query_parameters=query_params
+        )
+
+        messages = await self.user_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
+            request_configuration=request_config)
+
+        return messages
+
+    ####################################################################################################
+    async def send_mail(self, subject: str, body: str, recipient: str):
+        message = Message()
+        message.subject = subject
+
+        message.body = ItemBody()
+        message.body.content_type = BodyType.Text
+        message.body.content = body
+
+        to_recipient = Recipient()
+        to_recipient.email_address = EmailAddress()
+        to_recipient.email_address.address = recipient
+        message.to_recipients = []
+        message.to_recipients.append(to_recipient)
+
+        request_body = SendMailPostRequestBody()
+        request_body.message = message
+
+        await self.user_client.me.send_mail.post(body=request_body)
+
+    async def get_users(self):
+        query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
+            # Only request specific properties
+            select=['displayName', 'id', 'mail', 'content'],
+            # Get at most 25 results
+            top=25,
+            # Sort by display name
+            orderby=['displayName']
+        )
+        request_config = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
+            query_parameters=query_params
+        )
+
+        users = await self.user_client.users.get(request_configuration=request_config)
+        return users
+
+    async def get_attachments(self, message_id, ):
+        attachments = await self.user_client.me.messages.by_message_id(message_id).attachments.get()
+        return attachments
+
+    async def get_attachment_content(self, message_id, attachment_id):
+        attachments_content = await self.user_client.me.messages.by_message_id(message_id).attachments.by_attachment_id(
+            attachment_id).get()
+
+        return attachments_content
+
+    async def move_mail(self):
+        request_body = MovePostRequestBody(destination_id="Arch MS", )
+
+        move_mail = await self.user_client.me.messages.by_message_id('message_id').move.post(request_body)
+
+        return move_mail
---
+++ main.py
@@ -0,0 +1,188 @@
+import asyncio
+import configparser
+from msgraph.generated.models.o_data_errors.o_data_error import ODataError
+from graph import Graph
+import os
+import base64
+
+
+async def main():
+    print('Python Graph Tutorial\n')
+
+    # Load settings
+    config = configparser.ConfigParser()
+    config.read(['config.cfg', 'config.dev.cfg'])
+    azure_settings = config['azure']
+
+    graph: Graph = Graph(azure_settings)
+
+    await greet_user(graph)
+
+    choice = -1
+
+    while choice != 0:
+        print('Please choose one of the following options:')
+        print('0. Exit')
+        print('1. Display access token')
+        print('2. List my inbox')
+        print('3. Send mail')
+        print('4. List users')
+        print('5. Get attachments')
+
+        try:
+            choice = int(input())
+        except ValueError:
+            choice = -1
+
+        try:
+            if choice == 0:
+                print('Goodbye...')
+            elif choice == 1:
+                await display_access_token(graph)
+            elif choice == 2:
+                await list_inbox(graph)
+            elif choice == 3:
+                await send_mail(graph)
+            elif choice == 4:
+                await list_users(graph)
+            elif choice == 5:
+                await attachment_save(graph)
+            else:
+                print('Invalid choice!\n')
+        except ODataError as odata_error:
+            print('Error:')
+            if odata_error.error:
+                print(odata_error.error.code, odata_error.error.message)
+
+
+async def greet_user(graph: Graph):
+    CurrentUser = await graph.get_user()
+    if CurrentUser:
+        print('Hello,', CurrentUser.display_name)
+        # For Work/school accounts, email is in mail property
+        # Personal accounts, email is in userPrincipalName
+        print('Email:', CurrentUser.mail or CurrentUser.user_principal_name, '\n')
+
+
+async def display_access_token(graph: Graph):
+    token = await graph.get_user_token()
+    print('User token:', token, '\n')
+
+
+async def list_inbox(graph: Graph):
+    message_page = await graph.get_inbox()
+    if message_page and message_page.value:
+        # Output each message's details
+        for message in message_page.value:
+            print('Message:', message.subject)
+            if (
+                    message.from_ and
+                    message.from_.email_address
+            ):
+                print('  From:', message.from_.email_address.name or 'NONE')
+            else:
+                print('  From: NONE')
+            print('  Status:', 'Read' if message.is_read else 'Unread')
+            print('  Received:', message.received_date_time)
+            print(' Attachments', message.has_attachments)
+        # If @odata.nextLink is present
+        more_available = message_page.odata_next_link is not None
+        print('\nMore messages available?', more_available, '\n')
+
+
+async def send_mail(graph: Graph):
+    # Send mail to the signed-in user
+    # Get the user for their email address
+    user = await graph.get_user()
+    if user:
+        user_email = user.mail or user.user_principal_name
+
+        await graph.send_mail('Testing Microsoft Graph', 'Hello world!', user_email or '')
+        print('Mail sent.\n')
+
+
+async def list_users(graph: Graph):
+    users_page = await graph.get_users()
+    # Output each users' details
+    if users_page and users_page.value:
+        for user in users_page.value:
+            print('User:', user.display_name)
+            print('ID:', user.id)
+            print('Email:', user.mail)
+            print('department:', user.department)
+            print('Location:', user.office_location)
+
+        # If @odata.nextLink is present
+        more_available = users_page.odata_next_link is not None
+        print('\nMore users available?', more_available, '\n')
+
+
+async def attachment_save(graph: Graph):
+    message_page = await graph.get_inbox()
+
+    if message_page and message_page.value:
+        for message in message_page.value:
+            # check if not read
+            if not message.is_read:
+                # check for attachments
+                if message.has_attachments:
+                    print(message.subject, 'has attachments!')
+
+                    # Get the attachments for the current message
+                    attachments_response = await graph.get_attachments(message.id)
+                    attachments = attachments_response.value  # Access the 'value' property
+
+                    if attachments:
+                        for attachment in attachments:
+                            if ".pdf" in attachment.name.lower():
+                                attachment_name = attachment.name
+                                attachment_id = attachment.id
+                                attachment_content_type = attachment.content_type
+
+                                # Retrieve the attachment content (basics)
+                                print('Attachment Name:', attachment_name)
+                                print('Attachment ID:', attachment_id)
+                                print('Attachment Type:', attachment_content_type)
+                                # Retrieve the attachment content (FileAttachment)
+                                if "application/pdf" in attachment.content_type:
+                                    print(message.subject, "contains a True PDF")
+                                    attachment_content = await graph.get_attachment_content(message.id, attachment.id)
+                                    print('Attachment bytes:', attachment_content)
+
+                                    # decode the content
+
+                                    attachment_content_decoded = base64.b64decode(attachment_content.content_bytes)
+
+                                    # direct to directory
+                                    save_path = r'C:\Users\JobeB\Desktop\MSGDownloads'
+                                    file_name = attachment.name
+                                    file_path = os.path.join(save_path, file_name)
+
+                                    # Check if the file already exists
+                                    counter = 1
+                                    while os.path.exists(file_path):
+                                        # If the file already exists
+                                        file_name_without_extension, file_extension = os.path.splitext(attachment.name)
+                                        new_file_name = f"{file_name_without_extension}_{counter}{file_extension}"
+                                        file_path = os.path.join(save_path, new_file_name)
+                                        print(file_path)
+                                        counter += 1
+
+                                    # Save the attachment
+                                    print("Saving to File Path:", file_path)
+                                    with open(file_path, 'wb') as file:
+                                        file.write(bytes(attachment_content_decoded))
+                                    print(f"Attachment saved successfully at: {file_path}")
+
+                                else:
+                                    print(message.subject, "'s Attachment is not a PDF")
+                    else:
+                        print(message.subject, 'has no attachments')
+                else:
+                    print(message.subject, "has no attachments")
+            else:
+                print(message.subject, "has already been read")
+
+
+# Run main
+asyncio.run(main())
